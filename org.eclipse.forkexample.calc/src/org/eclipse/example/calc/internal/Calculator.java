53dd566cbe85920e20203555c63569bcf0d1e453
Daniel Rottmann <daniel.rottmann@fiduciagad.de>
2020-01-15 13:15:49 +0100

    newFeatureBranch

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -38,6 +38,7 @@ public class Calculator {
 	private void setupDefaultOperations() {
 		//more operations to come
 		// work directly in master
+		//newFeatureBranch
 		new Equals();
 		new Minus();
 		new Plus();
