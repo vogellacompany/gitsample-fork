900a5769b538b5774c7fad29eacaf13a9a0b358c
Silvan Hauke <silvan.hauke@fiduciagad.de>
2020-01-31 13:20:02 +0100

    github2

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -36,7 +36,7 @@ public class Calculator {
 	}
 
 	private void setupDefaultOperations() {
-		// more operations to come HeyHoLetsGo
+		// more operations to come HeyHoLetsGo 2
 		new Equals();
 		new Minus();
 		new Plus();
