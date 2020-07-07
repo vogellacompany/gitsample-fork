89344e1a968c49c120ad9539bd7ce41d3aa0b712
Andreas Telschow <Andreas.Telschow@fiduciagad.de>
2020-07-07 11:56:59 +0200

    Feature Divide / Multiply

---
+++ org.eclipse.forkexample.calc.test/tst/org/eclipse/example/calc/internal/operations/MultiplyTest.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.example.calc.internal.operations;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.example.calc.BinaryOperation;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MultiplyTest extends AbstractOperationTest {
+
+	private BinaryOperation op;
+
+	@Before
+	public void setUp() throws Exception {
+		op = new Multiply();
+	}
+
+	@Test
+	public void testPerform() {
+		assertEquals(4.0, op.perform(2.0F, 2.0F), 0.01F);
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals(op.getName(), "*");
+	}
+}
--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -13,8 +13,10 @@ import org.eclipse.example.calc.BinaryOperation;
 import org.eclipse.example.calc.Operation;
 import org.eclipse.example.calc.Operations;
 import org.eclipse.example.calc.UnaryOperation;
+import org.eclipse.example.calc.internal.operations.Divide;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
+import org.eclipse.example.calc.internal.operations.Multiply;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
 
@@ -40,6 +42,8 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new Multiply();
+		new Divide();
 	}
 
 	private void calculate(String cmdName) {
