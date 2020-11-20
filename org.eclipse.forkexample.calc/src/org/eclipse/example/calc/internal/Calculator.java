5295352bbd34a47126b3017accfa8293d7255b5f
Duerhagen <duerhagen.friedrich@scheidt-bachmann-st.de>
2020-11-20 11:50:27 +0100

    Kommentar hinzugefügt

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -39,6 +39,7 @@ public class Calculator {
 	    // Das ist der erste Kommentar in masterGithub
 	    // Das ist der zweite Kommentar in master
 	    // auch noch ein Kommentar von Ruth
+        //Jetzt mach ich auch noch ein Kommentar
 		new Equals();
 		new Minus();
 		new Plus();
