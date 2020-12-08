931e595a58c86572da76f9177cb48f887ae676c3
Julia Usselmann <jusselmann@dgverlag.de>
2020-12-08 12:19:20 +0100

    comment

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -39,7 +39,7 @@ public class Calculator {
 	    // Das ist der erste Kommentar in masterGithub
 	    // Das ist der zweite Kommentar in master
 	    // auch noch ein Kommentar von Ruth
-		// das ist ein Kommentar von masterGithub jus
+		// das ist ein Kommentar von masterGithub ju
 		new Equals();
 		new Minus();
 		new Plus();
