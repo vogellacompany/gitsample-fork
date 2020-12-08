6d45913aaf82bc6fcc3e702d23fce13620dc2482
Kai Herrgen <kherrgen@dgverlag.de>
2020-12-08 13:30:34 +0100

    Nur BlaBla...

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
@@ -11,12 +11,13 @@ package org.eclipse.example.calc;
  * Binary operation interface
  */
 public interface BinaryOperation extends Operation {
-	/**
-	 * hier fehlt eine beschreibung immer noch
-	 * @param arg1 first parameter
-	 * @param arg2 second parameter
-	 * 
-	 * @return result of binary operation 
-	 */ 
-	public float perform(float arg1, float arg2);
+    /**
+     * hier fehlt eine beschreibung immer noch
+     * @param arg1 first parameter
+     * @param arg2 second parameter
+     * 
+     * @return result of binary operation 
+     */ 
+    public float perform(float arg1, float arg2);
+    // BlaBlaBlaBla...
 }
