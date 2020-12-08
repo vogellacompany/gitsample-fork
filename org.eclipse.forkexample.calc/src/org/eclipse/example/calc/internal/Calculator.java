1532ad191fe07477b6789c59ce7d59b730aede42
Sascha Loetz <sloetz@dgverlag.de>
2020-12-08 12:41:06 +0100

    Killerfeature: x2
    Verdoppelung für doppelten Erfolg!

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -17,6 +17,7 @@ import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
+import org.eclipse.example.calc.internal.operations.XTwo;
 
 public class Calculator {
 
@@ -43,6 +44,7 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new XTwo();
 	}
 
 	private void calculate(String cmdName) {
