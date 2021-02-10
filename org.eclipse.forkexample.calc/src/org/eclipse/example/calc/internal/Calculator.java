f72d7c09c6eed6694234077a3e6278764825180d
xcg4640 <xcg4640@CP16769.inhouse.ad.in.gad.de>
2021-02-10 12:08:44 +0100

    Änderung

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -39,7 +39,7 @@ public class Calculator {
 	    // Das ist der erste Kommentar in masterGithub
 	    // Das ist der zweite Kommentar in master
 	    // auch noch ein Kommentar von Ruth
-		//Anpassung zu merken 2
+		//Anpassung zu merken 3
 		new Equals();
 		new Minus();
 		new Plus();
