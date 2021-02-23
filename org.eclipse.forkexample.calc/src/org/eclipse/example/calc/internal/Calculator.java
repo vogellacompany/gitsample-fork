8a5493beaed18342b783cddcfa1f3e202cccdc75
xgadpew <Peter.Wimber@fiduciagad.de>
2021-02-23 11:43:40 +0100

    neuer Kommentar

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -19,7 +19,7 @@ import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
 
 public class Calculator {
-
+// Testkommentar
 	private TextProvider textProvider;
 
 	private String cmd;
