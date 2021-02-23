91bd5d2649f35647d6ce0cc638a08a7334dfcc4b
xgadrns <xgadrns@CP11466.inhouse.ad.in.gad.de>
2021-02-23 11:38:54 +0100

    Feedback zum Hinweis

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -20,7 +20,7 @@ import org.eclipse.example.calc.internal.operations.Square;
 
 public class Calculator {
 	// DU KANNST EH NICHT RECHNEN!
-
+	// EY DAS IST GEMEIN!
 	private TextProvider textProvider;
 
 	private String cmd;
